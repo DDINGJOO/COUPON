bfe99f8f107092e819e4c6a896d87fd14b7f3479
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-02 19:50:18 +0900

    [FEAT] UpdateCouponPolicyRemainingQuantityCommand 구현
    - 수정 요청 커맨드 객체 추가
    - 입력 값 검증 로직 포함
    - 수정 사유 필드 추가
    
    Refs: #55

---
+++ springProject/src/main/java/com/teambind/coupon/application/port/in/UpdateCouponPolicyRemainingQuantityCommand.java
@@ -0,0 +1,39 @@
+package com.teambind.coupon.application.port.in;
+
+import com.teambind.coupon.common.SelfValidating;
+import lombok.Builder;
+import lombok.Value;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+
+/**
+ * 쿠폰 정책 남은 발급 수량 수정 커맨드
+ */
+@Value
+@Builder
+public class UpdateCouponPolicyRemainingQuantityCommand extends SelfValidating<UpdateCouponPolicyRemainingQuantityCommand> {
+
+    @NotNull(message = "쿠폰 정책 ID는 필수입니다.")
+    Long couponPolicyId;
+
+    @PositiveOrZero(message = "발급 수량은 0 이상이어야 합니다.")
+    Integer newMaxIssueCount; // null은 무제한을 의미
+
+    @NotNull(message = "수정자 정보는 필수입니다.")
+    Long modifiedBy;
+
+    String reason; // 수정 사유 (선택적)
+
+    public UpdateCouponPolicyRemainingQuantityCommand(
+            Long couponPolicyId,
+            Integer newMaxIssueCount,
+            Long modifiedBy,
+            String reason) {
+        this.couponPolicyId = couponPolicyId;
+        this.newMaxIssueCount = newMaxIssueCount;
+        this.modifiedBy = modifiedBy;
+        this.reason = reason;
+        this.validateSelf();
+    }
+}
